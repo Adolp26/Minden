a2fc68b3dcf34ba4cd3b7a3403680013c57fe2e7
Adolphe Ribeiro <adolpheribeiro7@gmail.com>
2024-11-10 23:41:34 -0300

    update middleware

--- api-minden/src/middlewares/authMiddleware.ts
+++ api-minden/src/middlewares/authMiddleware.ts
@@ -2,45 +2,44 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/constants';
 
-interface TokenPayload {
-  id: number;
-  email: string;
-  role: string;
-}
-
 // Extende a interface Request para incluir a propriedade user
 interface CustomRequest extends Request {
   user?: {
     id: number;
     email: string;
-    role: string;
   };
 }
 
-export const authMiddleware = (roles: string[]) => {
-  return (req: CustomRequest, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
+export const authMiddleware = (req: CustomRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-      return res.status(401).json({ message: 'Acesso não autorizado. Token não encontrado.' });
-    }
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Acesso não autorizado. Token não encontrado.' });
+  }
 
-    const token = authHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1];
+  
+  // Verificar se o token tem o formato correto (Bearer <token>)
+  if (!token) {
+    return res.status(400).json({ message: 'Token mal formatado.' });
+  }
 
-    try {
-      const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload;
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { id: number; email: string };
 
-      // Adiciona um console.log para ver o conteúdo do token decodificado
-      console.log('JWT Decodificado:', decoded); 
+    // Adiciona um console.log para ver o conteúdo do token decodificado
+    console.log('JWT Decodificado:', decoded);
 
-      if (!roles.includes(decoded.role)) {
-        return res.status(403).json({ message: 'Acesso proibido. Permissões insuficientes.' });
-      }
-
-      req.user = { id: decoded.id, email: decoded.email, role: decoded.role }; // Adiciona o usuário ao objeto de request
-      next();
-    } catch (error) {
+    req.user = { id: decoded.id, email: decoded.email }; // Adiciona o usuário ao objeto de request
+    next();
+  } catch (error) {
+    // Tratar diferentes tipos de erros
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: 'Token expirado.' });
+    } else if (error instanceof jwt.JsonWebTokenError) {
       return res.status(401).json({ message: 'Token inválido.' });
+    } else {
+      return res.status(500).json({ message: 'Erro interno no servidor.' });
     }
-  };
+  }
 };
